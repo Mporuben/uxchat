88437a365f82227e6933f799e3f9c5f19c014d6b
Matus Poruben <matus.poruben@gmail.com>
2026-06-15 21:39:02 +0200

    setup logto module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/content', 'nuxt-quasar-ui'],
+  modules: ['@nuxt/content', 'nuxt-quasar-ui', '@logto/nuxt'],
   devtools: { enabled: true },
   compatibilityDate: '2024-04-03',
+  app: {
+    head: {
+      title: 'UX Chat', // default fallback title
+      htmlAttrs: {
+        lang: 'en',
+      },
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+  },
   quasar: {
     plugins: ['BottomSheet', 'Dialog', 'Loading', 'LoadingBar', 'Notify', 'Dark'],
     extras: {
       font: 'roboto-font',
+    },
+  },
+  runtimeConfig: {
+    logto: {
+      endpoint: process.env.NUXT_LOGTO_ENDPOINT,
+      appId: process.env.NUXT_LOGTO_APP_ID,
+      appSecret: process.env.NUXT_LOGTO_APP_SECRET,
+      cookieEncryptionKey: process.env.NUXT_LOGTO_COOKIE_ENCRYPTION_KEY,
     },
   },
 });
