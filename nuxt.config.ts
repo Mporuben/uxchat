4f7c821124b27d55c14d609b1c94a221fb458fa8
Matus Poruben <matus.poruben@gmail.com>
2026-06-15 20:23:20 +0200

    setup nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/content',
-  ],
+  modules: ['@nuxt/content', 'nuxt-quasar-ui'],
   devtools: { enabled: true },
   compatibilityDate: '2024-04-03',
-})
+  quasar: {
+    plugins: ['BottomSheet', 'Dialog', 'Loading', 'LoadingBar', 'Notify', 'Dark'],
+    extras: {
+      font: 'roboto-font',
+    },
+  },
+});
